5b5fbcc03ca9802cdac3161bf091593b994fd523
costar011 <dnaer456@gmail.com>
2021-08-09 18:29:00 +0900

    stu

--- students/admin.py
+++ students/admin.py
@@ -5,3 +5,17 @@ from . import models
 @admin.register(models.StuModel)
 class StuModel(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.StuMemberModel)
+class StuMemberModel(admin.ModelAdmin):
+    """ StuMember Model Definition """
+
+    list_stu = (
+        "name",
+        "gender",
+        "view_mobile",
+        "email",
+    )
+
+    list_gender = ("gender",)
--- students/models.py
+++ students/models.py
@@ -13,3 +13,39 @@ class StuModel(models.Model):
 
     class Meta:
         verbose_name_plural = "STUDENTS"
+
+
+class StuMemberModel(models.Model):
+    """ StuMember Model Definition """
+
+    GENDER_MALE = "Male"
+    GENDER_FEMALE = "Female"
+    GENDER_OTHER = "Other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    name = models.CharField(max_length=30)
+    birth = models.DateField()
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10)
+    mobile = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
+
+    def view_mobile(self):
+        current_value = self.mobile
+        max_len = len(current_value)
+        first_mobile = current_value[0:3]
+        second_mobile = current_value[3:7]
+        third_mobile = current_value[7:max_len]
+
+        return f"{first_mobile}-{second_mobile}-{third_mobile}"
+
+    view_mobile.short_desciprtion = "MOBILE"
+
+    class Meta:
+        verbose_name_plural = "STUMEMBER"
